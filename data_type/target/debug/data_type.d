40130fbacb644f51f236a288273d481d3966d713
jwz <3544346335@qq.com>
2023-12-05 16:12:31 -0500

    float number

---
+++ data_type/target/debug/data_type.d
@@ -0,0 +1,1 @@
+/home/jwz/projects/Rust_learning/data_type/target/debug/data_type: /home/jwz/projects/Rust_learning/data_type/src/main.rs
---
+++ data_type/target/debug/deps/data_type-a43538a6d4578100.d
@@ -0,0 +1,5 @@
+/home/jwz/projects/Rust_learning/data_type/target/debug/deps/data_type-a43538a6d4578100: src/main.rs
+
+/home/jwz/projects/Rust_learning/data_type/target/debug/deps/data_type-a43538a6d4578100.d: src/main.rs
+
+src/main.rs:
