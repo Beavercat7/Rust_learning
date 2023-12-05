efbbca8da10b07a05392f11716cd9f58ac550035
jwz <3544346335@qq.com>
2023-12-05 11:01:47 -0500

    constant

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,4 +3,5 @@ fn main() {
    println!("The value of x is: {}",x);
    x = 6;
    println!("The value of x is: {}",x);
+   const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
 }
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/jwz/projects/Rust_learning/variables/target/debug/variables: /home/jwz/projects/Rust_learning/variables/src/main.rs
