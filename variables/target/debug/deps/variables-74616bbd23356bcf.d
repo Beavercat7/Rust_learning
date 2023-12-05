44c063a1f6745daf621fc2a9b178f3d11f134245
jwz <3544346335@qq.com>
2023-12-05 10:45:48 -0500

    immutable variable

---
+++ variables/target/debug/deps/variables-74616bbd23356bcf.d
@@ -0,0 +1,5 @@
+/home/jwz/projects/Rust_learning/variables/target/debug/deps/variables-74616bbd23356bcf: src/main.rs
+
+/home/jwz/projects/Rust_learning/variables/target/debug/deps/variables-74616bbd23356bcf.d: src/main.rs
+
+src/main.rs:
